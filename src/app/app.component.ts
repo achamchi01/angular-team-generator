3efa0a9439c3b47baa3d4da7618fe35d5e43afbe
ayoub <47029182+ayoubuv@users.noreply.github.com>
2021-11-06 20:49:13 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+
+  newMemberName: string = "";
+  members: string[] = [];
+
+  numberOfTeams = 0;
+  teams:string[][] = [];
+
+  errorMsg = "";
+
+  addNewMember(){
+    if(this.newMemberName == ""){
+      this.errorMsg = "Member Name Can't be Empty";
+      return;
+    }
+    this.members.push(this.newMemberName);
+    this.newMemberName = "";
+    this.errorMsg = "";
+  }
+
+  generateTeams(){
+
+    if(!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMsg = "Invalid Number Of Teams";
+      return;
+    }
+
+    if(this.members.length < this.numberOfTeams){
+      this.errorMsg = "Not Enough Members";
+      return;
+    }
+
+    this.errorMsg = "";
+    this.teams = [];
+
+    while(this.members.length > 0){
+      for(let i = 0; i < this.numberOfTeams; i++){
+        const index = Math.floor(Math.random()*this.members.length);
+        if(this.members[index]){
+          if(!this.teams[i]) this.teams[i] = [];
+          this.teams[i].push(this.members[index]);
+          this.members.splice(index, 1);
+        }else break;
+      }
+    }
+    console.log(this.teams)
+  }
+
 }
